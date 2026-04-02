194174ae064a2115339d7b9ae322c8c45bce1f4e
Calvin Bayer <calvin.bayer@datadoghq.com>
2026-04-02 13:00:44 +0200

    fix: Use external datadog-ci with checked-in node_modules
    The datadog-ci plugin architecture requires plugins to be discoverable
    in node_modules at runtime. Bundling with ncc doesn't work with this
    architecture.
    
    Solution:
    - Mark @datadog/datadog-ci as external in ncc build
    - Check in node_modules/ (standard practice for GitHub Actions)
    - Keep yarn.lock for dependency locking
    
    This provides supply chain security through:
    - Locked dependency versions (yarn.lock)
    - Verified dependencies (checked-in node_modules)
    - No runtime npm/npx installations
    
    Trade-off: 109MB node_modules in repo (necessary for plugin system)

---
+++ node_modules/@datadog/datadog-ci-base/dist/helpers/errors.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.InvalidConfigurationError = void 0;
+class InvalidConfigurationError extends Error {
+}
+exports.InvalidConfigurationError = InvalidConfigurationError;
+//# sourceMappingURL=errors.js.map
